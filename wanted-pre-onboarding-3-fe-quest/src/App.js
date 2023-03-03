cb92e031c3236a2c64b090f711810192fd2501e0
Mina <00minha@gmail.com>
2023-03-03 18:00:43 +0900

    feature: 기본 html 구조 추가

--- wanted-pre-onboarding-3-fe-quest/src/App.js
+++ wanted-pre-onboarding-3-fe-quest/src/App.js
@@ -3,6 +3,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <header>Wanted Pre-onboarding course</header>
+      <article>
+        <nav>
+          <ul className="pageMenu">
+            <li>Page A</li>
+            <li>Page B</li>
+            <li>Page C</li>
+          </ul>
+        </nav>
+        <section>page contents</section>
+      </article>
+      
+      
     </div>
   );
 }
